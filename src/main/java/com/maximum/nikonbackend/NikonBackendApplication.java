2a70a53cc83197a509fcdec23e8998154403b2a9
maximum <1291185069@qq.com>
2024-06-15 03:20:21 +0800

    feat: 项目初始化

---
+++ src/main/java/com/maximum/nikonbackend/NikonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.maximum.nikonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NikonBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maximum/nikonbackend/NikonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maximum.nikonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikonBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
