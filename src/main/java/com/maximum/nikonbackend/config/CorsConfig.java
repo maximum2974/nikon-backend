6acf888e3491aa85eae5da9072b333a617bfe67f
maximum <1291185069@qq.com>
2024-06-26 18:37:11 +0800

    fix: ...

--- src/main/java/com/maximum/nikonbackend/config/CorsConfig.java
+++ src/main/java/com/maximum/nikonbackend/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://45.145.228.53:5173")
                 .allowCredentials(true)
                 .allowedOriginPatterns("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
