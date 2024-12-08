d578e72943fac432b4eff5b81ba4653bf34d3c82
maximum <1291185069@qq.com>
2024-12-08 08:31:37 +0800

    fix: ...

--- src/main/java/com/maximum/nikonbackend/config/WebMvcConfig.java
+++ src/main/java/com/maximum/nikonbackend/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("*")
                 .allowCredentials(true)
                 .allowedMethods("*")
                 .maxAge(3600);
